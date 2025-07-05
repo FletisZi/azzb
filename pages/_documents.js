2798a14a6071ef9bdd8a8f8b81a3b4515b6910e7
Rodrigo Santos <rjrodrigosantos12@gmail.com>
2025-07-05 17:38:24 -0300

    update _documents.js

--- pages/_documents.js
+++ pages/_documents.js
@@ -1,10 +1,19 @@
-export default function _document () {
-    return(
-        <head>
-            <meta charset="UTF-8">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <link rel="stylesheet" href="../styles/global.css" />
-            <title>Document</title>
-        </head>
-    )
+// pages/_document.js
+import { Html, Head, Main, NextScript } from 'next/document';
+
+export default function Document() {
+  return (
+    <Html lang="pt-BR">
+      <Head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="stylesheet" href="/styles/global.css" />
+        <title>azzab</title>
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
 }
